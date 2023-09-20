abebda7c475b33a0763a3701c72b07e9a7ee73ab
Lee Youngmin <80088671+youngandmini@users.noreply.github.com>
2023-09-21 00:26:28 +0900

    Time: 56 ms (94.65%), Space: 17.5 MB (56.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -7,18 +7,11 @@ class Solution:
         
         for i in range(len(nums)):
             
-            # 이미 중복된 값이 있으면
-            if nums[i] in dictionary:
-                # 곱하기 2한 값을 타겟과 비교하여 조기종료
-                if nums[i] * 2 == target:
-                    return [i, dictionary[nums[i]]]
+            pair = target - nums[i]
+            #이미 짝이 있다면 반환
+            if pair in dictionary:
+                return [i, dictionary[pair]]
+            
             else:
                 dictionary[nums[i]] = i
-        
-        #반대 짝을 찾음
-        for i in dictionary:
-            pair = target - i
-            
-            if i != pair and pair in dictionary:
-                return [dictionary[i], dictionary[pair]]
         
