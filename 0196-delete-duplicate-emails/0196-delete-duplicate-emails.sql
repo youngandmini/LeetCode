cf2d7322097f263cb41d84d607704a23cc127085
Lee Youngmin <80088671+youngandmini@users.noreply.github.com>
2023-11-30 21:33:37 +0900

    Time: 1195 ms (89.16%), Space: 0B (100.00%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+
+delete 
+from Person 
+where id not in (
+    select *
+    from (
+        select min(id)
+        from Person
+        group by email) as p)
+    
